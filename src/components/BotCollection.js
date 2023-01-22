80a05372a546322dbb54073fd8cad7680d7d5979
Jobwawesh <jobnjunge15@gmail.com>
2023-01-22 15:20:08 +0300

    edited bots files

---
+++ src/components/BotCollection.js
@@ -0,0 +1,22 @@
+import React from "react";
+import BotItem from "./BotItem";
+
+function BotCollection({ collection, clickHandler, handleDelete }) {
+  // Your code here
+  return (
+    <div className="">
+      {"Bot Collection click on a bot to add it to your army."}
+      <div className="">
+        {collection.map((bot) => (
+          <BotCard 
+          key={bot.id} 
+          bot={bot} 
+          clickHandler={clickHandler} 
+          handleDelete={handleDelete} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default BotCollection;
---
+++ src/components/BotItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function BotItem(){
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default BotItem
